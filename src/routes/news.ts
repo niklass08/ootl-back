ff39cb1df51d1993a12488987d9a74de9bf61537
Nicolas Babel <n.babel@lectra.com>
2020-09-24 13:45:29 +0200

    feat: caching news for one hour

--- src/routes/news.ts
+++ src/routes/news.ts
@@ -5,6 +5,10 @@ import { NewsApiArticle, NewsApiResponse } from '../api/newsApi';
 import { mapNewsApiResponseToNews } from '../mappers/newsApiToNews';
 import News from '../News';
 import config from '../config';
+import NodeCache from 'node-cache';
+const NEWS_KEY = 'NEWS';
+
+const newsCache = new NodeCache();
 
 const newsAPI = new NewsAPI(config.api.newsApi.key);
 const router = express.Router();
@@ -13,18 +17,31 @@ router.get('/', (req, res) => {
   res.send('News Route');
 });
 
-const getRandomTopic = () => {
-  const topics = Object.keys(constants.topics);
-  return topics[Math.floor(Math.random() * topics.length)];
-};
-
-const requestRandomFreshNews = async (): Promise<News> => {
-  const topic = getRandomTopic();
+const getTopicNews = async (topic: string): Promise<News[]> => {
   const response: NewsApiResponse = await newsAPI.v2.topHeadlines({
     category: topic,
     country: 'fr'
   });
-  const news: News[] = response.articles.map(mapNewsApiResponseToNews);
+  return response.articles.map(mapNewsApiResponseToNews);
+};
+
+const getNews = async (): Promise<News[]> => {
+  let news: News[] = [];
+  for (const topic of Object.keys(constants.topics)) {
+    const topicNews = await getTopicNews(topic);
+    news = news.concat(topicNews);
+  }
+  return news;
+};
+const requestRandomFreshNews = async (): Promise<News> => {
+  let news = newsCache.get(NEWS_KEY) as News[];
+  if (news) {
+    console.log('returning cached data');
+    return news[Math.floor(Math.random() * news.length)];
+  }
+  news = await getNews();
+  newsCache.set(NEWS_KEY, news, 60 * 60);
+  console.log('returning fresh data');
   return news[Math.floor(Math.random() * news.length)];
 };
 
